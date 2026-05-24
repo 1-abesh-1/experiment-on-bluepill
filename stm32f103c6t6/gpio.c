be4962d24bc6d2e1f8446a0b23058a9fcd531e4e
Sadman Sakib Abesh <sadmansakibabesh@gmail.com>
2026-05-24 23:35:44 +0600

    Implement GPIO configuration and control functions

---
+++ stm32f103c6t6/gpio.c
@@ -0,0 +1,61 @@
+#include "stm32f103c6t6/gpio.h"
+
+void ConfigRCC(GPIO_TypeDef *GPIOx){
+  if(GPIOx==GPIOA)  RCC->APB2ENR |=(1<<2);
+   if(GPIOx==GPIOB)  RCC->APB2ENR |=(1<<3);
+    if(GPIOx==GPIOC)  RCC->APB2ENR |=(1<<4);
+};
+
+void ConfigOutput(
+    GPIO_TypeDef *GPIOx,
+    uint8_t pin
+){
+volatile uint32_t *config;
+
+if(pin<8){
+    config=&GPIOx->CRL;
+}else{
+
+        config=&GPIOx->CRH;
+        pin-=8;
+    }
+
+    *config &= ~(0xF<<(pin*4));
+
+*config |= (0x2<<(pin*4));
+
+
+};
+
+
+void Write(
+    GPIO_TypeDef *GPIOx,
+    uint8_t pin,
+    uint8_t state
+){
+
+    if(state){
+        GPIOx->BSRR=(1<<pin);
+    }else{
+        GPIOx->BRR=(1<<pin);
+    }
+
+
+};
+
+void Toggle(
+    GPIO_TypeDef *GPIOx,
+    uint8_t pin
+){
+GPIOx->ODR ^=(1<<pin);
+};
+
+
+
+void delay(uint32_t ms)
+{
+    for(uint32_t i = 0; i < (ms * 8000); i++)
+    {
+        __asm__("nop");
+    }
+}
